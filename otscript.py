0b3e2bd8dfe0fec40166f996ef8e08b3b039f628
Sébastien Tran Tien <trantien@math.univ-lyon1.fr>
2021-09-02 16:28:01 +0200

    version du 01/09 14h27
    J'ai continué sur le code de Pedro. Un petit point qui me gênait est, quand dans le cas test1.txt, linprog échangeait des gens de hauteur différentes (resp. 3 et 4). Je pense que c'est dû au fait que les hauteurs de sont pas assez différentes (écart trop peu important), donc, comme Kiki, j'ai modifié les hauteurs (cas test modifié directement dans le code en PJ) pour que l'écart soit au moins de 5cm. Dans ce cas, linprog n'échange pas les gens de hauteurs différentes.
    
    Ensuite j'ai pris un autre cas test dans la détection 001 (c'est le jeu de données C dans la PJ), plus de points, hauteurs de l'ordre de 170cm, mais toujours pas de pieds et toujours nbNuage1 = nbNuage2. J'ai aussi modifié un chouia le code à plusieurs endroits pour la portabilité et l'affichage. Que pensez-vous du résultat avec le jeu de données C ? Je trouve ça cohérent.
    
    Maintenant j'essaie avec nbNuage1 et nbNuage2 différents. Il faut utiliser A_ub et b_ub du coup je pense
    
    Remarque : on ne peut pas voir les différences dans git car j'ai remplacé toutes les lignes du script python. Je n'ai pas remplacé que les lignes que j'avais changées par rapport à la version précédente, pour gagner du temps.

--- otscript.py
+++ otscript.py
@@ -5,31 +5,61 @@ import matplotlib.pyplot as plt
 #=======================================================#
                    #- DONNEES -#
 #=======================================================#
-x1 = [4, 151, -23, 1]
-x2 = [4, 153, 25, 2]
-x3 = [4, 158, 94, 3]
-x4 = [4, 158, 92, 4]
-x5 = [4, 155, 86, 5]
-y1 = [5, 154, -26, 1]
-y2 = [5, 153, 25, 2]
-y3 = [5, 158, 92, 3]
-y4 = [5, 158, 94, 4]
-y5 = [5, 155, 85, 5]
+
+'''Pour l'instant, on n'utilise que des données "jouet" fabriquées par nous-mêmes'''
+
+#Jeu de données A
+
+# x1 = [4, 151, -23, 1]
+# x2 = [4, 153, 25, 2]
+# x3 = [4, 158, 94, 3]
+# x4 = [4, 158, 92, 4]
+# x5 = [4, 155, 86, 5]
+# y1 = [5, 154, -26, 1]
+# y2 = [5, 153, 25, 2]
+# y3 = [5, 158, 92, 3]
+# y4 = [5, 158, 94, 4]
+# y5 = [5, 155, 85, 5]
+# X = np.array([x1,x2,x3,x4,x5])
+# Y = np.array([y1,y2,y3,y4,y5])
+
+#RESULTAT : les tailles sont toutes distinctes, et pourtant, linprog envoie x3 sur y4 et x4 sur y3. Peut-être les écarts entre les tailles ne sont-ils pas assez grands ?
+
+#Jeu de données B
+
+# x1 = [4, 151, -23, 5]
+# x2 = [4, 153, 25, 10]
+# x3 = [4, 158, 94, 15]
+# x4 = [4, 158, 92, 20]
+# x5 = [4, 155, 86, 25]
+# y1 = [5, 154, -26, 5]
+# y2 = [5, 153, 25, 10]
+# y3 = [5, 158, 92, 15]
+# y4 = [5, 158, 94, 20]
+# y5 = [5, 155, 85, 25]
+# X = np.array([x1,x2,x3,x4,x5])
+# Y = np.array([y1,y2,y3,y4,y5])
+
+#RESULTAT : on n'a changé, par rapport au jeu de données A, que les tailles. Les écarts sont plus significatifs, de l'ordre de 10 cm. Alors, les tailles ne sont plus mélangées dans le résultat. J'interprète ça comme un point positif : on n'a pas à distinguer deux personnes si leurs tailles diffèrent de quelques centimètres, mais on arrive à la distinguer si leurs tailles diffèrent de 10cm.
+
+#Jeu de données C (issu de la détection 001 ; pas de pieds ; nbPtsNuage1 = nbPtsNuage2)
+
+X = np.array([[116, -89, -94, 123], [116, 114, -119, 82], [116, 122, -116, 90], [116, 55, -67, 136], [116, 57, -67, 136], [116, 107, -34, 137], [116, -39, -23, 119], [116, 158, 92, 104], [116, 155, 85, 117], [116, 158, 94, 103]])
+Y = np.array([[117, -74, -84, 136], [117, 115, -108, 100], [117, -76, -74, 137], [117, 58, -48, 141], [117, -84, -49, 90], [117, 107, -17, 135], [117, -38, 2, 120], [117, 158, 92, 104], [117, 155, 86, 118], [117, 158, 94, 103]])
 
 #=======================================================#
                    #-MATRICE DE COUT-#
 #=======================================================#
-X = np.array([x1,x2,x3,x4,x5])
-Y = np.array([y1,y2,y3,y4,y5])
-
 
 def distance(u,v):
     return(np.linalg.norm(u[1:]-v[1:]))
 
-C = np.zeros([5,5])
+nbPtsNuage1 = X.shape[0]
+nbPtsNuage2 = Y.shape[0]
+C = np.zeros([nbPtsNuage1, nbPtsNuage2])
 
-for i in range(5):
-    for j in range(5):
+for i in range(nbPtsNuage1):
+    for j in range(nbPtsNuage2):
         C[i,j] = distance(X[i],Y[j])
 
 c = np.ravel(C) #lignes de C
@@ -42,8 +72,6 @@ c = np.ravel(C) #lignes de C
                  #-MATRICE DE COUPLAGE-#
 #=======================================================#
 
-nbPtsNuage1 = X.shape[0]
-nbPtsNuage2 = Y.shape[0]
 nbVariables = nbPtsNuage1*nbPtsNuage2 # En général c'est de la meme taile que c.
 nbContraintesSortantes = nbPtsNuage1
 nbContraintesEntrantes = nbPtsNuage2
@@ -80,15 +108,19 @@ x = np.array([P.x[k*nbPtsNuage2:(k+1)*nbPtsNuage2] for k in range(nbPtsNuage1)])
 #x est la matrice de connexion, cad la matrice des pij
 print(x)
 
-
 #=======================================================#
                  #-TRACE DES SOLUTIONS-#
 #=======================================================#
 
 plt.clf()
 #tracé des deux nuages de points initiaux
-plt.scatter(X[:,1], X[:,2], marker = '+')
-plt.scatter(Y[:,1], Y[:,2], marker = 'x')
+plt.scatter(X[:,1], X[:,2], marker = '+', color = 'blue')
+plt.scatter(Y[:,1], Y[:,2], marker = 'x', color = 'red')
+#annotation des hauteurs de chaque point
+for i in range(nbPtsNuage1):
+    plt.annotate("h =" + str(X[i,3]), (X[i,1], X[i,2]), color = 'blue', fontsize = 7)
+for j in range(nbPtsNuage2):
+    plt.annotate("h =" + str(Y[j,3]), (Y[j,1], Y[j,2]), color = 'red', fontsize = 7)
 #tracé des connexions
 for i in range(nbPtsNuage1):
     for j in range(nbPtsNuage2):
