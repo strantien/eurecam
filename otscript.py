49e1eb77f2cd21fc5a61afe6c3cfe51deee30286
Sébastien Tran Tien <trantien@math.univ-lyon1.fr>
2021-09-05 15:41:31 +0200

    creation trajectoires

--- otscript.py
+++ otscript.py
@@ -14,128 +14,10 @@ os.chdir("/home/trantien/Bureau/icj/doctorat/challenge_amies/Challenge_AMIES_EUR
 #os.chdir("pedro")
 #os.chdir("kiki")
 
-##Jeu de données A (nbPtsNuage1 = nbPtsNuage2)
-
-# x1 = [4, 151, -23, 1]
-# x2 = [4, 153, 25, 2]
-# x3 = [4, 158, 94, 3]
-# x4 = [4, 158, 92, 4]
-# x5 = [4, 155, 86, 5]
-# y1 = [5, 154, -26, 1]
-# y2 = [5, 153, 25, 2]
-# y3 = [5, 158, 92, 3]
-# y4 = [5, 158, 94, 4]
-# y5 = [5, 155, 85, 5]
-# X = np.array([x1,x2,x3,x4,x5])
-# Y = np.array([y1,y2,y3,y4,y5])
-
-#RESULTAT : les tailles sont toutes distinctes, et pourtant, linprog envoie x3 sur y4 et x4 sur y3. Peut-être les écarts entre les tailles ne sont-ils pas assez grands ?
-
-##Jeu de données B (nbPtsNuage1 = nbPtsNuage2)
-
-# x1 = [4, 151, -23, 5]
-# x2 = [4, 153, 25, 10]
-# x3 = [4, 158, 94, 15]
-# x4 = [4, 158, 92, 20]
-# x5 = [4, 155, 86, 25]
-# y1 = [5, 154, -26, 5]
-# y2 = [5, 153, 25, 10]
-# y3 = [5, 158, 92, 15]
-# y4 = [5, 158, 94, 20]
-# y5 = [5, 155, 85, 25]
-# X = np.array([x1,x2,x3,x4,x5])
-# Y = np.array([y1,y2,y3,y4,y5])
-
-#RESULTAT : on n'a changé, par rapport au jeu de données A, que les tailles. Les écarts sont plus significatifs, de l'ordre de 10 cm. Alors, les tailles ne sont plus mélangées dans le résultat. J'interprète ça comme un point positif : on n'a pas à distinguer deux personnes si leurs tailles diffèrent de quelques centimètres, mais on arrive à la distinguer si leurs tailles diffèrent de 10cm.
-
-##Jeu de données C (issu de la détection 001, nbPtsNuage1 = nbPtsNuage2)
-
-# X = np.array([[116, -89, -94, 123], [116, 114, -119, 82], [116, 122, -116, 90], [116, 55, -67, 136], [116, 57, -67, 136], [116, 107, -34, 137], [116, -39, -23, 119], [116, 158, 92, 104], [116, 155, 85, 117], [116, 158, 94, 103]])
-# Y = np.array([[117, -74, -84, 136], [117, 115, -108, 100], [117, -76, -74, 137], [117, 58, -48, 141], [117, -84, -49, 90], [117, 107, -17, 135], [117, -38, 2, 120], [117, 158, 92, 104], [117, 155, 86, 118], [117, 158, 94, 103]])
-
-#RESULTAT : plutôt cohérent, non ?
-
-##Jeu de données D (nbPtsNuage1 > nbPtsNuage2)
-
-# x1 = [4, 151, -23, 5]
-# x2 = [4, 153, 25, 10]
-# x3 = [4, 158, 94, 15]
-# x4 = [4, 158, 92, 20]
-# x5 = [4, 155, 86, 25]
-# x6 = [4, 155, 70, 30]
-# y1 = [5, 154, -26, 5]
-# y2 = [5, 153, 25, 10]
-# y3 = [5, 158, 92, 15]
-# y4 = [5, 158, 94, 20]
-# y5 = [5, 155, 85, 25]
-# X = np.array([x1,x2,x3,x4,x5,x6])
-# Y = np.array([y1,y2,y3,y4,y5])
-
-#RESULTAT : ok, x6 est ignoré (pas de connexion partant de x6).
-
-##Jeu de données E (nbPtsNuage1 < nbPtsNuage2)
-
-# x1 = [4, 151, -23, 5]
-# x2 = [4, 153, 25, 10]
-# x3 = [4, 158, 94, 15]
-# x4 = [4, 158, 92, 20]
-# x5 = [4, 155, 86, 25]
-# y1 = [5, 154, -26, 5]
-# y2 = [5, 153, 25, 10]
-# y3 = [5, 158, 92, 15]
-# y4 = [5, 158, 94, 20]
-# y5 = [5, 155, 85, 25]
-# y6 = [5, 153, 70, 30]
-# X = np.array([x1,x2,x3,x4,x5])
-# Y = np.array([y1,y2,y3,y4,y5,y6])
-
-#RESULTAT : ok, y6 est ignoré (pas de connexion arrivant vers x6).
-
-##Jeu de données E de Kyriaki (nbPtsNuage1 = nbPtsNuage2)
-
-# df = pd.read_csv("testKiki.txt",sep=" ",names=["Image", "x", "y", "z", "height"])
-# df.drop(0,0,inplace=True)
-# #on transforme le tableau obtenu en un tableau d'entiers
-# data = np.vectorize(int)(np.asarray(df))
-# #je supprime la colonne des z ; sinon, il faut modifier la définition de distance
-# data = np.delete(data, obj = 3, axis = 1)
-# #print(data)
-# X = data[:5,:]
-# Y = data[5:,:]
-
-##Jeu de données F : détection 001, transport entre l'image i et l'image i+1
-
-# #la façon d'extraire le fichier et de construire le tableau d'indices est un peu pourrie. Je n'avais pas vu que c'était déjà fait dans show_detection.
-#
-# df = pd.read_csv("file://"+os.getcwd()+"/data_detection/001/detection.txt",sep=" ",names=["Image", "x", "y", "z", "height"],skiprows=2)
-# df.drop(0,0,inplace=True)
-#
-# #on transforme le tableau obtenu (strings) en un tableau d'entiers : data[:,0] contient les numéros d'images, qui doivent être entiers
-# data = np.vectorize(int)(np.asarray(df))
-#
-# #on crée un tableau d'indices à 2 colonnes, dont la ligne i donne les indices min et max des points de l'image numéro i + imageMin
-# #par exemple, indices[0, :] = [0, 4]
-# imageMin, imageMax = np.min(data[:,0]), np.max(data[:,0])
-# indices = np.zeros([imageMax - imageMin + 1, 2], dtype = int)
-# imageCourante = imageMin
-# jmin = 0
-# j = jmin
-# while imageCourante < imageMax:
-#     if data[j, 0] == imageCourante:
-#         j += 1
-#     else: #dans ce cas, data[j, 0] > imageCourante
-#         indices[imageCourante - imageMin, :] = [jmin, j-1]
-#         imageCourante += 1
-#         jmin = j
-#         j += 1
-#
-# i = 114
-# X = data[indices[i-imageMin, 0]:indices[i-imageMin, 1]] #image i
-# Y = data[indices[i-imageMin+1, 0]:indices[i-imageMin+1, 1]] #image i+1
+from trajectories_new import trajectory
+from tuerTrajectoires import *
 
 ##Jeux de données du sujet
-
-#copier-coller de show_detection.py
 
 dataset = "001"
 # dataset = "002"
@@ -298,22 +180,89 @@ def plotTransport(X, Y):
 #plotTransport(X, Y)
 
 #=======================================================#
-                 #-FILM COMPLET-#
+                 #-TRAJECTOIRES-#
 #=======================================================#
+
+def createTrajInit():
+    XInit = detections[(detections["#image"]==images[0])][:].values
+    trajInit = [[XInit[i].tolist()] for i in range(XInit.shape[0])]
+    for i in images[:-1]:
+        iNext = images[int(np.argwhere(images == i))+1]
+        maskY = (detections["#image"]==iNext)
+        Y = detections[maskY][:].values
+        trajInit = trajectory(trajInit, Y)
+    return(trajInit)
+
+trajectories = createTrajInit()
+
+for k in range(len(trajectories)):
+    np.savetxt('trajectory' + str(k) +'.txt', trajectories[k])
+
+#list to be plotted = deadTrajectories = [t1, t2, ...]
+
+#=======================================================#
+                 #-FILM AVEC TRACE ENTRE i ET i+1-#
+#=======================================================#
+
+# plt.ion()
+# fig = plt.figure()
+# for i in images[:-1]:
+#     print("==> image : ",i)
+#     iNext = images[int(np.argwhere(images == i))+1] #oui, c'est sale
+#     maskX = (detections["#image"]==i)
+#     maskY = (detections["#image"]==iNext)
+#     X =  detections[maskX][:].values
+#     Y = detections[maskY][:].values
+#     couplage = computeTransport(X, Y)
+#     nbPtsNuage1 = X.shape[0]
+#     nbPtsNuage2 = Y.shape[0]
+#
+#     plt.clf()
+#
+#     try:
+#         im = imageio.imread(imgdir+"/image-"+str(i).zfill(3)+".png")
+#     except:
+#         im = imageio.imread(imgdir+"/image-"+str(i).zfill(3)+".jpg")
+#
+#     height, width  = im.shape
+#
+#     ixX = 0.5 * (X[:,1] / X[:,3] * f + cx)
+#     iyX = 0.5 * (X[:,2] / X[:,3] * f + cy)
+#     ixY = 0.5 * (Y[:,1] / Y[:,3] * f + cx)
+#     iyY = 0.5 * (Y[:,2] / Y[:,3] * f + cy)
+#
+#     plt.imshow(im)
+#     plt.scatter(ixX, iyX, marker="+",color="blue")
+#     plt.scatter(ixY, iyY, marker="x",color="red")
+#     for i in range(nbPtsNuage1):
+#         for j in range(nbPtsNuage2):
+#             if couplage[i,j] == 1.:
+#                 plt.plot([ixX[i], ixY[j]], [iyX[i], iyY[j]], color = 'pink', linewidth = .7)
+#     plt.pause(0.5)  #plt.pause(0.05) #input('type enter to continue')
+#     plt.draw()
+#
+# plt.ioff()
+# plt.show()
+
+#=======================================================#
+                 #-FILM AVEC TRAJECTOIRES COMPLETES-#
+#=======================================================#
+
+# t1 = [(4,1,2,1,9),(5,1,1,1,9),(6,0.5,0.5,1,9),(7,0.2,0.2,1,9), (8,0.1,0.2,1,9)]
+# t2 = [(4,2,2,2,8),(5,1,1,2,8),(6,2.1,0.5,2,8),(7,2,2,2,8),(8,1,1,2,8),(9,2.1,0.5,2,8),(10,2,2,2,8),(11,1,1,2,8)]
+
+# t1 = [[4.0, 151.0, -23.0, 179.0, 1.0], [5.0, 154.0, -26.0, 183.0, 1.0] ,[6, 152, -41, 287, 103]]
+# t2 = [[4.0, 153.0, 25.0, 174.0, 2.0], [5.0, 153.0, 25.0, 174.0, 2.0], [6, 152, -27, 286, 107], [7, 158, 92, 174, 1]]
+#
+# trajectories = [t1, t2]
+#
+
+colors = ['red', 'pink', 'blue', 'green', 'grey']
 
 plt.ion()
 fig = plt.figure()
-for i in images[:-1]:
+for i in images:
     print("==> image : ",i)
-    iNext = images[int(np.argwhere(images == i))+1] #oui, c'est sale
-    maskX = (detections["#image"]==i)
-    maskY = (detections["#image"]==iNext)
-    X =  detections[maskX][:].values
-    Y = detections[maskY][:].values
-    couplage = computeTransport(X, Y)
-    nbPtsNuage1 = X.shape[0]
-    nbPtsNuage2 = Y.shape[0]
-
     plt.clf()
 
     try:
@@ -323,19 +272,19 @@ for i in images[:-1]:
 
     height, width  = im.shape
 
-    ixX = 0.5 * (X[:,1] / X[:,3] * f + cx)
-    iyX = 0.5 * (X[:,2] / X[:,3] * f + cy)
-    ixY = 0.5 * (Y[:,1] / Y[:,3] * f + cx)
-    iyY = 0.5 * (Y[:,2] / Y[:,3] * f + cy)
-
     plt.imshow(im)
-    plt.scatter(ixX, iyX, marker="+",color="blue")
-    plt.scatter(ixY, iyY, marker="x",color="red")
-    for i in range(nbPtsNuage1):
-        for j in range(nbPtsNuage2):
-            if couplage[i,j] == 1.:
-                plt.plot([ixX[i], ixY[j]], [iyX[i], iyY[j]], color = 'pink', linewidth = .7)
-    plt.pause(0.5)  #plt.pause(0.05) #input('type enter to continue')
+
+    for traj in trajectories:
+        for k in range(len(traj)-1):
+            if traj[k][0] == i:  #si la trajectoire part de l'image i
+                ix_trajk = 0.5 * (traj[k][1] / traj[k][3] * f + cx)
+                iy_trajk = 0.5 * (traj[k][2] / traj[k][3] * f + cy)
+                ix_trajkPlus1 = 0.5 * (traj[k+1][1] / traj[k+1][3] * f + cx)
+                iy_trajkPlus1 = 0.5 * (traj[k+1][2] / traj[k+1][3] * f + cy)
+                plt.plot([ix_trajk, ix_trajkPlus1], [iy_trajk, iy_trajkPlus1], color = colors[trajectories.index(traj)], linewidth = .4)
+                plt.arrow(ix_trajkPlus1, iy_trajkPlus1, 5, 5, shape='full', lw=0, length_includes_head=True, head_width=.05, color = colors[trajectories.index(traj)])
+    plt.pause(1.5)
+    #input('type enter to continue')
     plt.draw()
 
 plt.ioff()
