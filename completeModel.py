1f2a5ae43e97f06260b9c56db8ca5887faabd10e
Sébastien Tran Tien <trantien@math.univ-lyon1.fr>
2021-09-06 21:21:01 +0200

    Update completeModel.py

--- completeModel.py
+++ completeModel.py
@@ -10,6 +10,7 @@ os.chdir("/home/trantien/Bureau/icj/doctorat/challenge_amies/Challenge_AMIES_EUR
 
 import traitement as t
 import optimalTransport as ot
+import affichage
 
 
 #==============================================================================#
@@ -52,10 +53,10 @@ nbPastVelocities0 = 3 #vitesses qu'on va considerer pour faire des moyennes des 
 nbTrajectoires0 = 10 # nb de trajectoires qu'on va considerer pour quand on fait de stat sur l'ensemble de trajectoires mortes
 duplicateTolerance0 = 20 #tolerance de la "distance" pour differencier 2 personnes dans une meme image.
 speedTolerance0 = 10 # tolerance qu'on utlise quand on compare des vitesses entre eux.
-baseLifeTime0 = 1000 # Temps de vie max qu'on considere raissonable quand on n'a pas de donnés. (unités: en frames)
+baseLifeTime0 = 10 # Temps de vie max qu'on considere raissonable quand on n'a pas de donnés. (unités: en frames)
 lifeTimeTolerance0 = 10 # Tolerance pour comparer le temps de vie moyennes des trajectoires mortes.
-directionChangeTolerance0 = 4 # Tolerance  pour comparer le nombre de changements de sense d'une trajectoire.
-noiseEqTolerance0 = 10 # Tolerance utlisée pour montrer egalité entre un point d'une image et un point qu'on sait c'est du bruit.
+directionChangeTolerance0 = 2 # Tolerance  pour comparer le nombre de changements de sense d'une trajectoire.
+noiseEqTolerance0 = 30 # Tolerance utlisée pour montrer egalité entre un point d'une image et un point qu'on sait c'est du bruit.
 #==============================================================================#
 #                      Initialization
 #==============================================================================#
@@ -72,67 +73,79 @@ trajectoiresActives0 = [t.nouveauTrajet(p) for p in trajets1] # liste de traject
 #==============================================================================#
 # first function of trajectories when P is constructed only with equalities-inequalites with 1
 def trajectory(trajets,Y):
-    X = np.array([ t[-1] for t in trajets])
-    P = ot.computeTransport(X, Y)
-    nbPtsX = X.shape[0]
-    nbPtsY = Y.shape[0]
-    Ylist=[]
-    Xlist=[]
-    newTrajets=[]
-    if nbPtsX<=nbPtsY:
-        #Nous réarrangeons le nuage de points Y
-        #Ceux qui sont en trop on les oublie
-        for i in range(nbPtsX):
-            for j in range(nbPtsY):
-                if P[i,j]==1:
-                    Ylist.append(tuple(Y[j,:]))
-        #On colle les points (dans le bon ordre) Y avec chaque trajectoire de X
-        for i in range(len(trajets)):
-            trajets[i].append(Ylist[i])
-            newTrajets.append(trajets[i])
-        #Le reste de Points dans Y sont oublies en tant que 'bruit'
+    if len(trajets) == 0 and Y.shape[0] == 0:
+        return([])
+    if len(trajets) == 0 and Y.shape[0] > 0:
+        Ylist = list(Y)
+        return([t.nouveauTrajet(p) for p in Ylist])
+    if len(trajets) > 0 and Y.shape[0] == 0:
+        return(trajets)
     else:
-        #Dans ce cas, nous réarrangeons les trajectoires dans trajets au lieu des points dans Y.
-        Xcote = [] # Ici nous allons mettre les trajectoires que ne seront pas connectées avec un point dans Y
-        Ylist = [tuple(p) for p in list(Y)]
-        for i in range(nbPtsX):
-            for j in range(nbPtsY):
-                if P[i,j]==1:
-                    Xlist.append(trajets[i]) #Nous rearrangeons les trajectoires. Il faut quelles matchent l'ordre de Y
-            if sum([P[i,j] for j in range(nbPtsY)]) == 0:
-                Xcote.append(trajets[i]) # Nous prenons celles qui n'ont pas matché dans cette liste (on les oublie pas!)
-        #Ici nous allons coller les points de Ylist et les trajectoires pertinentes.
-        for i in range(nbPtsY):
-            Xlist[i].append(Ylist[i])
-            newTrajets.append(Xlist[i])
-        #Ici nous rajoutons les trajectoires qu'on na pas connecté á des points dans Y.
-        newTrajets = newTrajets + Xcote
-    return(newTrajets)
+        X = np.array([ t[-1] for t in trajets])
+        P = ot.computeTransport(X, Y)
+        nbPtsX = X.shape[0]
+        nbPtsY = Y.shape[0]
+        Ylist=[]
+        Xlist=[]
+        newTrajets=[]
+        if nbPtsX<=nbPtsY:
+            #Nous réarrangeons le nuage de points Y
+            #Ceux qui sont en trop on les oublie
+            for i in range(nbPtsX):
+                for j in range(nbPtsY):
+                    if P[i,j]==1:
+                        Ylist.append(tuple(Y[j,:]))
+            #On colle les points (dans le bon ordre) Y avec chaque trajectoire de X
+            for i in range(len(trajets)):
+                trajets[i].append(Ylist[i])
+                newTrajets.append(trajets[i])
+            #Le reste de Points dans Y sont oublies en tant que 'bruit'
+        else:
+            #Dans ce cas, nous réarrangeons les trajectoires dans trajets au lieu des points dans Y.
+            Xcote = [] # Ici nous allons mettre les trajectoires que ne seront pas connectées avec un point dans Y
+            Ylist = [tuple(p) for p in list(Y)]
+            for i in range(nbPtsX):
+                for j in range(nbPtsY):
+                    if P[i,j]==1:
+                        Xlist.append(trajets[i]) #Nous rearrangeons les trajectoires. Il faut quelles matchent l'ordre de Y
+                if sum([P[i,j] for j in range(nbPtsY)]) == 0:
+                    Xcote.append(trajets[i]) # Nous prenons celles qui n'ont pas matché dans cette liste (on les oublie pas!)
+            #Ici nous allons coller les points de Ylist et les trajectoires pertinentes.
+            for i in range(nbPtsY):
+                Xlist[i].append(Ylist[i])
+                newTrajets.append(Xlist[i])
+            #Ici nous rajoutons les trajectoires qu'on na pas connecté á des points dans Y.
+            newTrajets = newTrajets + Xcote
+        return(newTrajets)
 
 #==============================================================================#
 #                      Cleaning Process
 #==============================================================================#
 #First we clean the points in the next frame "Y" (c'est un array)
 def cleanPoints(Y, noiseList, duplicateTol, noiseEqTolerance): #Prende un array et donne un array comme réponse (et la nouvelle liste de noise!)
-    noNoiseY = []
-    YClean = []
-    for p in (Y.shape([0])) :
-        if t.isNoise(Y[p], noiseList, noiseEqTolerance):
-            tuplePoint = tuple(Y[p])
-            noiseList.append(tuplePoint)
+    if Y.shape[0] == 0:
+        return(Y, noiseList)
+    else:
+        noNoiseY = []
+        YClean = []
+        for p in range(Y.shape[0]) :
+            if t.isNoise(Y[p], noiseList, noiseEqTolerance):
+                tuplePoint = tuple(Y[p])
+                noiseList.append(tuplePoint)
+            else:
+                tuplePoint = tuple(Y[p])
+                noNoiseY.append(tuplePoint)
+        noNoiseY = np.array(noNoiseY)
+        if noNoiseY.shape[0] == 0:
+            return(Y, noiseList)
         else:
-            tuplePoint = tuple(Y[p])
-            noNoiseY.append(tuplePoint)
-    noNoiseY = np.array(noNoiseY)
-    YClean.append(tuple(noNoiseY[0]))
-    for i in (noNoiseY.shape([0])):
-        if t.isIn(noNoise[i], YClean, t.peopleEqualityFunc, duplicateTol):
-            #On fait rien!
-        else:
-            pointTuple = tuple(noNoiseY[i])
-            YClean.append(pointTuple)
-    YClean = np.array(YClean)
-    return(YClean, noiseList)
+            YClean.append(tuple(noNoiseY[0]))
+            for i in range(noNoiseY.shape[0]):
+                if not t.isIn(noNoiseY[i], YClean, t.peopleEqualityFunc, duplicateTol):
+                    pointTuple = tuple(noNoiseY[i])
+                    YClean.append(pointTuple)
+            YClean = np.array(YClean)
+            return(YClean, noiseList)
 #Now we clean trajectories. This means killing the ones that are leaving and eliminating the false ones.
 def cleanTrajectories(Trajectories, noiseList, deadTrajectories,speedTolerance, nbPastVelocities, lifeTimeTolerance, baseLifeTime, DirectionChangeTolerance):
   stillAlive = []
@@ -150,13 +163,15 @@ def cleanTrajectories(Trajectories, noiseList, deadTrajectories,speedTolerance, 
 #==============================================================================#
 #                      Iteration
 #==============================================================================#
-for i in images[:20]:
+for i in images[:-1]:
     iNext = images[int(np.argwhere(images == i))+1]
     maskY = (detections["#image"]==iNext)
     Y = detections[maskY][:].values # np.array
     #clean les points
     (Y,noiseList0) = cleanPoints(Y, noiseList0, duplicateTolerance0, noiseEqTolerance0)
     #clean trajectoires
-    (trajectoiresActives0,noiseList0, trajectoiresMortes0) = cleanTrajectories(trajectoiresActives0, noiseList0, trajectoiresMortes0, speedTolerance0, nbPastVelocities0, lifeTimeTolerance0, baseLifeTime0, DirectionChangeTolerance0)
+    (trajectoiresActives0,noiseList0, trajectoiresMortes0) = cleanTrajectories(trajectoiresActives0, noiseList0, trajectoiresMortes0, speedTolerance0, nbPastVelocities0, lifeTimeTolerance0, baseLifeTime0, directionChangeTolerance0)
     #Linking Process
     trajectoiresActives0 = trajectory(trajectoiresActives0, Y)
+
+affichage.plotComplete(dataset, trajectoiresMortes0)
