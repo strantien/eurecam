35d8e18400c77d28306ce65b59ae5e005d43c8c5
Pedro Jaramillo <53344147+Safepepe@users.noreply.github.com>
2021-09-09 11:54:48 +0200

    Update traitement.py

--- traitement.py
+++ traitement.py
@@ -1,151 +1,187 @@
 import numpy as np
 import math as m
-import optimalTransport as ot
+
+#====================MAIN OBJECTS====================+#
+# point wil be a 5-tuple: (frame, x, y, z, h)
+# trajectory state will be a n-tuple (n => 5): (frame,x,y,z,h,velocity,meanVelocity)
+# trajectory will be a list of trajectory states
+#====================================================+#
 
 #------------------------------------------------------------------------------#
 #                          Auxilary Functions
 #------------------------------------------------------------------------------#
+def dirChange(velocity1, velocity2):#Takes in 2 tuples and returns a Bool
+    (vx1,vy1) = velocity1[:2]
+    (vx2,vy2) = velocity2[:2]
+    change = np.sign(vx1*vx2 + vy1*vy2)
+    abruptChange = change <= 0
+    return(abruptChange)
 
+def meanOfTuples(tuples):# takes a non-empty list of n-tuples, returns an n-tuple
+   n = len(tuples)
+   tab = np.array(tuples)
+   mean = np.sum(tab,axis=0)*(1/n)
+   mean = tuple(mean)
+   return(mean)
 
-def vitesses(trajectoire): #Prend une liste de tuples. Returns a list of tuples [(vx,vy)].
-    nPoints = len(trajectoire)
-    if nPoints < 2:
-        return([])
-    else:
-        trajectoireArray = np.array(trajectoire) #turn it into an array
-        endPs = trajectoireArray[1:]
-        startPs = trajectoireArray[:-1]
-        vitessesX = (endPs[:,1] - startPs[:,1])/(endPs[:,0]-startPs[:,0])
-        vitessesY = (endPs[:,2] - startPs[:,2])/(endPs[:,0]-startPs[:,0])
-        vs0 = np.stack((vitessesX,vitessesY), axis= 1)
-        vs1 = list(vs0)
-        vs = [tuple(v) for v in vs1]
-        return(vs)
+def stateToPoint(state):#Takes in tuple state and returns a point
+    point = list(state)[:5]
+    point = tuple(point)
+    return(point)
 
-def vitesseMoyenne(trajectoire, nPastVelocities): #prends une liste de tuples. Returns a tuple (vx,vy)
-    if nPastVelocities < 1 or len(trajectoire) < 2:
-        return((0,0)) # Choix arbitraire (mais raisonnable!)
-    else:
-        n = min(len(trajectoire) - 1, nPastVelocities)
-        vs0 = np.array(vitesses(trajectoire))
-        vs = np.flip(vs0 ,0)[:n]
-        moyenne0 = np.sum(vs0, axis=0)*(1/n)
-        moyenne = tuple(moyenne0)
-        return(moyenne)
+def newState(point): #Prends un tuple (point) et returns a tuple (trajectory state)
+   p = list(point)
+   lastVelocity = (0,0)
+   meanVelocity = (0,0)
+   p = p + [lastVelocity, meanVelocity]
+   p = tuple(p)
+   return(p)
+
+def trajectoryToListOfPoints(trajectory):
+    pointList = [stateToPoint(st) for st in trajectory]
+    return(pointList)
 
 def tempsDeVie(trajectoire): #Prend une liste de tuples. returns a number
-    framesArray= np.array(trajectoire)[:,0]
+    framesArray = np.array(trajectoryToListOfPoints(trajectoire))[:,0]
     return(framesArray[-1] - framesArray[0])
 
 def tempsDeVieMoyenne(trajectoires, nbTrajectoires):#Prend une liste de trajectoires. Returns a real value (positif)
-    n = len(trajectoires)
-    consideredTrajectories = trajectories[(n - nbTrajectoires):] # c'est une liste de trajectoires cet objet
-    listOfTimes = map(tempsDeVie, consideredTrajectories)
-    arrayOfTimes = np.array(listOfTimes)
-    totalTime = np.sum(arrayOfTimes)
-    return(totalTime/nbTrajectoires)
+    n = min(len(trajectoires) , nbTrajectoires)
+    k = len(trajectoires) - n
+    consideredTrajectories = trajectoires[k:] # c'est une liste de trajectoires cet objet
+    listOfTimes = [ tempsDeVie(traj) for traj in consideredTrajectories]
+    totalTime = sum(listOfTimes)
+    return(totalTime/n)
 
-def peopleEqualityFunc(p1,p2, equalityTolerance): #prends deux tuples de taille 5, returns Boolean.
-    (frame1,x1,y1,z1,h1) = p1
-    (frame2,x2,y2,z2,h2) = p2
-    areEqual = (m.sqrt((x2-x1)**2 + (y2-y1)**2 + min(min((h2-h1)**2,h2**2),h1**2))) < equalityTolerance
-    return(areEqual)
+def peopleEqualityFunc(trendTolerance,  velocityVector, p1, p2, equalityTolerance): #prends deux tuples. returns Boolean.
+    (vx, vy) = velocityVector
+    vNorm = m.sqrt(vx**2 + vy**2)
+    if vNorm > trendTolerance:
+            unitVelocityVector = (vx/vNorm, vy/vNorm)
+            (x1,y1,z1,h1) = tuple(p1[1:5])
+            (x2,y2,z2,h2) = tuple(p2[1:5])
+            dpNorm = m.sqrt((x2-x1)**2 + (y2-y1)**2)
+            (unitdpx,unitdpy) = ((x2-x1)/dpNorm, (y2-y1)/dpNorm)
+            (unitVx,unitVy) = unitVelocityVector
+            alignment = abs(unitdpx*unitVx + unitdpy*unitVy)
+            heightCost1 = (h2-h1)**2
+            heightCost2 = min(min((h2-h1)**2,h2**2),h1**2)
+            heightCost = (1-alignment)*heightCost1 + alignment*heightCost2
+            areEqual = (m.sqrt((x2-x1)**2 + (y2-y1)**2 + heightCost)) < equalityTolerance
+            return(areEqual)
+    else:
+        (x1,y1,z1,h1) = tuple(p1[1:5])
+        (x2,y2,z2,h2) = tuple(p2[1:5])
+        heightCost = min(min((h2-h1)**2,h2**2),h1**2)
+        areEqual = (m.sqrt((x2-x1)**2 + (y2-y1)**2 + heightCost)) < equalityTolerance
 
-def equalFuncNoise(p1,p2, equalityTolerance): #prends deux tuples de taille 5, returns Boolean.
-    (frame1,x1,y1,z1,h1) = p1
-    (frame2,x2,y2,z2,h2) = p2
+def equalFuncNoise(p1,p2, equalityTolerance): #prends deux tuples, returns Boolean.
+    (x1,y1,z1,h1) = tuple(p1[1:5])
+    (x2,y2,z2,h2) = tuple(p2[1:5])
     areEqual = (m.sqrt((x2-x1)**2 + (y2-y1)**2 + (h2-h1)**2)) < equalityTolerance
     return(areEqual)
 
-def isIn(point, pointList, equalityFunc, equalityTolerance): #Prends un tuple (5 valeurs) et une liste de tuples (5 valeurs). Returns a Boolean.
+def tupleIsIn(point, pointList, equalityFunc, equalityTolerance): #Prends un tuple et une liste de tuples. Returns a Boolean.
     equalityList = [equalityFunc(point, p, equalityTolerance) for p in pointList]
-    for isEqual in equalityList:
-        if isEqual:
+    for matches in equalityList:
+        if matches:
             return(True)
     return(False)
 
+def nouveauTrajet(point): # Prends un array 1D ou tuple. Returns a trajectory with one point.
+    newSt = newState(point)
+    newTrajet =[newSt]
+    return(newTrajet)
+
+def addPointToTraj(point,trajectory): #takes in a 5-tuple and a trajectory. Returns a trajectory
+    x = trajectory[-1][1]
+    y = trajectory[-1][2]
+    (meanVx, meanVy) = trajectory[-1][6]
+    newX = point[1]
+    newY = point[2]
+    n = len(trajectory)
+    dt = point[0] - trajectory[-1][0]
+    newVelocity = ((newX - x)/dt, (newY - y)/dt)
+    newMeanVelocity = ((n*meanVx + newVelocity[0])/(n+1), (n*meanVy + newVelocity[1])/(n+1))
+    newState = list(point)
+    newState = newState + [newVelocity, newMeanVelocity] #here I assume that "point" is a 5-tuple
+    newState = tuple(newState)
+    newTrajectory = trajectory + [newState]
+    return(newTrajectory)
 
 #------------------------------------------------------------------------------#
-#                     Functions applied on frame/image/nuage
+#                     Functions applied on frame/image/nuage/points
 #------------------------------------------------------------------------------#
-
 #totally arbitrary condition for now
-def isNewTrajectory(point, minBordX, maxBordX, minBordY, maxBordY, borderToleranceX, borderToleranceY): #Prends un tuple ou un np.array. Returns a Boolean.
-    (frame,x,y,z,h) = point
-    nearBorderX = (abs(x-minBordX) < borderToleranceX) | (abs(x-maxBordX) < borderToleranceX)
-    nearBorderY = (abs(y-minBordY) < borderToleranceY) | (abs(y-maxBordY) < borderToleranceY)
-    nearBorder = nearBorderX | nearBorderY
-    return(nearBorder)
 
 def isNoise(point, noiseList, noiseTol):#Prends un np.array ou un tuple, et une liste de tuples. returns a Boolean.
     #Something smarter can be done easily.
-    return(isIn(point,noiseList, equalFuncNoise, noiseTol))
+    return(tupleIsIn(point,noiseList, equalFuncNoise, noiseTol))
 
-def nouveauTrajet(point): # Prends un array 1D ou tuple (avec 5 elements exactement!). Returns a trajectory.
-    p = tuple(point)
-    (frame,x,y,z,h) = p
-    return([(frame,x,y,z,h)])
+def advanceOneFrameState(state, stationnary): # Prends un tuple . Returns a tuple
+    if stationnary:
+        frame = state[0]
+        newPoint = [(frame+1)]
+        n = len(state)
+        for i in range(n-1):
+            newPoint.append(state[i+1])
+        newPoint = tuple(newPoint)
+        return(newPoint)
+    else:
+        frame = state[0]
+        newState = [(frame+1)]
+        x = state[1]
+        y = state[2]
+        (vx,vy) = state[5]
+        (meanVx,meanVy) = state[6]
+        z = state[3]
+        h = state[4]
+        newX = x + vx
+        newY = y + vy
+        newState = newState + [newX,newY,z,h,(vx,vy),(meanVx,meanVy)]
+        newState = tuple(newState)
+        return(newState)
 #------------------------------------------------------------------------------#
 #                           Functions applied on trajectories
 #------------------------------------------------------------------------------#
-def leavesDomain(trajectoire, xMin,xMax,yMin,yMax, speedTolerance, nPastVs):
-    #prends une liste de tuples, des bounds, une tolerance et le nombre de Vitesses passées á regarder. Returns a Boolean
-    vs = vitesses(trajectoire)
-    vMoyenne = vitesseMoyenne(trajectoire, nPastVs)
-    if np.size(vs) < 1:
-        return(False)#trajectory just started and so it probably won't leave! We could take <2 as well.
-    else:
-        (frame,x,y,z,h) = trajectoire[-1] #last/current position
-        vxMoyenne = vMoyenne[0] #Mean velocity in x axis
-        vyMoyenne = vMoyenne[1] #Mean velocity in y axis
-        vxFinal   = vs[-1][0] #Last velocity in x axis
-        vyFinal   = vs[-1][1] #Last velocity in y axis
-        if vxFinal > speedTolerance: #moving right
-            slowingDown =  vxFinal < (vxMoyenne - speedTolerance)
-            if (abs(x-xMax) < abs(vxFinal)) & (not slowingDown):
-                return(True)
-        if vxFinal < - speedTolerance: #moving left
-            slowingDown =  vxFinal > (vxMoyenne + speedTolerance)
-            if (abs(x-xMin) < abs(vxFinal)) & (not slowingDown):
-                return(True)
-        if vyFinal > speedTolerance: #moving up
-            slowingDown =  vyFinal < (vyMoyenne - speedTolerance)
-            if (abs(y-yMax) < abs(vyFinal)) & (not slowingDown):
-                return(True)
-        if vyFinal < - speedTolerance: #moving down
-            slowingDown =  vyFinal > (vyMoyenne + speedTolerance)
-            if (abs(y-yMin) < abs(vyFinal)) & (not slowingDown):
-                return(True)
+
+def advanceOneFrameTraj(trajectory, stationnary):#takes a trajectory and a Bool. Returns a trajectory
+    lastState = trajectory[-1]
+    lastState = advanceOneFrameState(lastState,stationnary)
+    trajectory.append(lastState)
+    return(trajectory)
+
+def leavesDomain(trajectoire, xMin,xMax,yMin,yMax, speedTolerance): #prends une liste de states, des bounds, une tolerance. Returns a Boolean
+    lastState  = trajectoire[-1]
+    expectedState = advanceOneFrameState(lastState, False)
+    expectedX = expectedState[1]
+    expectedY = expectedState[2]
+    if (expectedX < (xMax - speedTolerance)) & (expectedX > (xMin + speedTolerance)) & (expectedY < (yMax - speedTolerance)) & (expectedY > (yMin + speedTolerance)):
         return(False)
+    return(True)
 
 def remainsTooLong(trajectoire, trajectoiresMortes, lifeTimeTolerance, baseLifeTime): #Prend une liste de tuples, liste de trajectoires, et deux reel. Returns a Boolean.
-    #Condition 1: Trajectory remains inside Domain for too long!
     nbMortes = len(trajectoiresMortes)
     if nbMortes < 1:
-        tempsDeVieMoyenne = 0
+        meanLifeTime = 0
     else:
-        tempsDeTrajectoiresMortes0 = [tempsDeVie(traj) for traj in trajectoiresMortes]
-        tempsDeTrajectoiresMortesArray = np.array(tempsDeTrajectoiresMortes0)
-        tempsDeVieMoyenne = np.sum(tempsDeTrajectoiresMortesArray)*(1/nbMortes)
-    if tempsDeVieMoyenne ==0:
+        meanLifeTime = tempsDeVieMoyenne(trajectoiresMortes, nbMortes)
+    if meanLifeTime == 0:
         return(tempsDeVie(trajectoire) > baseLifeTime)
     else:
-        return(tempsDeVie(trajectoire) > min(baseLifeTime,tempsDeVieMoyenne + lifeTimeTolerance))
+        return(tempsDeVie(trajectoire) > min(baseLifeTime, meanLifeTime + lifeTimeTolerance))
 
 def changesDirectionTooMuch(trajectoire, trajectoiresMortes, DirectionChangeTolerance):
-    #Condition 2: Trajectory changes direction too much (or too fast! --> to implement later)
-    vs0 = vitesses(trajectoire)
-    if len(vs0) == 0:
-        return(False)
-    else:
-        vs  = np.array(vs0)
-        vxs = np.sign(vs[:,0]) # direction of speeds in x axis
-        vys = np.sign(vs[:,1]) # directions of speeds in y axis
-        vxTransitions = vxs[:-1]*vxs[1:] # an array of {0,1,-1}, where -1 means a change in direction
-        vyTransitions = vys[:-1]*vys[1:] # an array of {0,1,-1}, where -1 means a change in direction
-        change_DirectionX = vxTransitions < 0
-        change_DirectionY = vyTransitions < 0
-        nbVxChanges = np.size(vxTransitions[change_DirectionX])
-        nbVyChanges = np.size(vyTransitions[change_DirectionY])
-        changesDirectionTooMuch = (nbVxChanges + nbVyChanges) > DirectionChangeTolerance
-        return(changesDirectionTooMuch)
+    vs = [state[5] for state in trajectoire]
+    startVs = vs[:-1]
+    endVs = vs[1:]
+    changes = []
+    for i in range(len(startVs)):
+        change = dirChange(startVs[i],endVs[i])
+        changes.append(change)
+    changeCount = 0
+    for ch in changes:
+        if ch:
+            changeCount = changeCount+1
+    tooMuch = changeCount > DirectionChangeTolerance
+    return(tooMuch)
